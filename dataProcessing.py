820c015733038e8d23b547a67214f284504e97bf
林禹澔 <142852296+haner0834@users.noreply.github.com>
2024-02-22 21:03:12 +0800

    Update dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -82,24 +82,6 @@ class Greeting:
         return text
     
 '''
-I'm going to process text of note to turn to Swift Language
-this is Data Structure in my Swift File
-struct CardContentItem: Identifiable {
-    let id = UUID()
-    let text: String
-    let image: Image?
-    let isShowButton: Bool?
-
-    init(text: String, image: Image? = nil, isShowButton: Bool? = nil) {
-        self.text = text
-        self.image = image
-        self.isShowButton = isShowButton
-    }
-}
-in my project, this structure will be in an array, which means I need to add "," to split each item
-and I added some image to Xcode, so the input of image name(Image  IMAGE_NAME)is the name of image
-after running this, I'll copy the output and paste into my Swift project
-
 
 rule of tagging greeting:
 GREETING                                            => when there's nothing to tag
